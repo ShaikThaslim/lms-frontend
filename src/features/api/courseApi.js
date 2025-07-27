4d353fc39c95edf6f8b08db0b293947967130381
ShaikThaslim <thaslimshaik800@gmail.com>
2025-07-27 16:46:38 +0530

    Use production backend URL from env variable

--- src/features/api/courseApi.js
+++ src/features/api/courseApi.js
@@ -1,6 +1,7 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const COURSE_API="http://localhost:8080/api/v1/course";
+const COURSE_API = import.meta.env.VITE_BACKEND_URL + "/api/v1/course";
+//"http://localhost:8080/api/v1/course"
 export const courseApi=createApi({
     reducerPath:"courseApi",
     tagTypes:['Refetch_Creator_Course','Refetch_Lecture'],
